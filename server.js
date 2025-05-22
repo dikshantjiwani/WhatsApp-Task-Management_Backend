5f78e306eb47e8bbb5e9def36a641b4619720cca
Dikshant Jiwani <dikshant.jiwani@gmail.com>
2025-05-22 16:57:05 +0530

    Added whatsapp webhook route to server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const taskRoutes = require('./routes/taskRoutes');
+const whatsappRoutes = require('./routes/whatsappRoutes');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
 app.use('/api', taskRoutes);
+app.use('/api', whatsappRoutes); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
